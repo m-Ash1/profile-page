82f645119fa23cd224591c774a596261e1137bf8
Mahmoud Ashraf <mashraffarouk2702@gmail.com>
2023-05-09 13:13:11 +0300

    minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import info from "./assets/json/all-info.json";
 import Card from "./components/Card";
 
 function App() {
-  // console.log(info.players[0]);
   return (
     <>
-      <Card info={info.players[0]}></Card>
+      <Card info={info.players[1]}></Card>
     </>
   );
 }
