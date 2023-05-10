bb82d1036e3c314efdb6e00cd7ccd557ef9d9506
Mahmoud Ashraf <mashraffarouk2702@gmail.com>
2023-05-10 12:27:22 +0300

    error 404 page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import info from "./assets/json/all-info.json";
 import Card from "./components/Card";
+import Error404 from "./components/Error404";
 
 function App() {
-  return (
-    <>
-      <Card info={info.players[0]}></Card>
-    </>
-  );
+  const isLoggedIn = true;
+  let content;
+  if (isLoggedIn) {
+    content = <Card info={info.players[0]}></Card>;
+  } else {
+    content = <Error404></Error404>;
+  }
+  return content;
 }
 
 export default App;
